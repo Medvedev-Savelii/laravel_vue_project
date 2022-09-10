62fc9e06cb57c66daeab41e25e13ef7ca86951ba
sava <savamedvedev@gmail.com>
2022-09-10 21:21:46 +0800

    five-commit(vue-basic-project)

---
+++ app/Http/Controllers/Person/StoreController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Person;
+use App\Http\Controllers\Controller;
+
+class StoreController extends Controller
+{
+    public function __invoke()
+    {
+
+        return $persons;
+    }
+}
