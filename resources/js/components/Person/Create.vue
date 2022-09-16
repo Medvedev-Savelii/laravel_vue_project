24b16e0d93ad448d149a504f3c1f1012a59635ec
sava <savamedvedev@gmail.com>
2022-09-16 18:29:01 +0800

    fourteen-commit

--- resources/js/components/Person/Create.vue
+++ resources/js/components/Person/Create.vue
@@ -10,12 +10,13 @@
             <input type="text" placeholder="job" v-model="job" class="form-control">
         </div>
         <div class="mb-3">
-            <input type="submit" value="Add" class="btn btn-primary">
+            <input @click.prevent="store" type="submit" value="Add" class="btn btn-primary">
         </div>
     </div>
 </template>
 
 <script>
+import router from "../../router.js";
 export default {
     name: "Create",
     data() {
@@ -23,6 +24,14 @@ export default {
             name: '',
             age: null,
             job: null
+        }
+    },
+    methods: {
+        store() {
+            axios.post('/api/people', {name: this.name, age: this.age, job: this.job})
+                .then(res => {
+                    router.push('/people')
+                })
         }
     },
 }
--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,10 +1,46 @@
 <template>
-    <div>Index</div>
+    <div class="w-50 p-3">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people">
+                <td>{{person.name}}</td>
+                <td>{{person.age}}</td>
+                <td>{{person.job}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+             people: null
+        }
+    },
+    mounted() {
+        this.getPeople();
+    },
+    methods: {
+        getPeople() {
+            axios.get('/api/people')
+                .then(res => {
+                    this.people = res.data
+                })
+        }
+    }
 }
 </script>
 
