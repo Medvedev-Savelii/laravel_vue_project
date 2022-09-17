91543eacd69cc16f02f17ef8670039cb55cc8eef
sava <savamedvedev@gmail.com>
2022-09-17 18:02:22 +0800

    sixteen-commit

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -6,13 +6,23 @@
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="person in people">
-                <td>{{person.name}}</td>
+                <td>
+                    <router-link class="text-dark text-decoration-none" :to="{name: 'person.show', params: {id: person.id}}">{{person.name}}</router-link>
+                </td>
                 <td>{{person.age}}</td>
                 <td>{{person.job}}</td>
+                <td>
+                    <button type="button" class="btn btn-primary"><router-link class="text-light text-decoration-none" :to="{ name: 'person.edit', params: {id: person.id}}">Edit</router-link></button>
+                </td>
+                <td>
+                    <a href="#" class="btn btn-outline-danger" @click.prevent="deletePerson(person.id)">Delete</a>
+                </td>
             </tr>
             </tbody>
         </table>
@@ -39,6 +49,11 @@ export default {
                 .then(res => {
                     this.people = res.data
                 })
+        },
+        deletePerson(id) {
+            axios.delete('/api/people/' + id)
+                .then(res => { this.getPeople()})
+                .catch(error => console.log(error))
         }
     }
 }
--- resources/js/components/Person/Show.vue
+++ resources/js/components/Person/Show.vue
@@ -1,10 +1,38 @@
 <template>
-    <div>Show</div>
+    <div v-if="person">
+        <div>
+            Name: {{this.person.name}}
+        </div>
+        <div>
+            Age: {{this.person.age}}
+        </div>
+        <div>
+            Job: {{this.person.job}}
+        </div>
+        <button type="button" class="btn btn-primary"><router-link class="text-black-50 text-decoration-none" :to="{ name: 'person.edit', params: {id: this.person.id}}">Edit</router-link></button>
+
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Show"
+    name: "Show",
+    data() {
+        return {
+            person: null
+        }
+    },
+    mounted() {
+        this.getPerson();
+    },
+    methods: {
+        getPerson() {
+            axios.get('/api/people/' + this.$route.params.id)
+                .then(res => {
+                    this.person = res.data
+                })
+        },
+    }
 }
 </script>
 
